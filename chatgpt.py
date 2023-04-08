c59036566b007c0da0b64fc0fe28698e3e03b436
森哥 <54741446+senge-dev@users.noreply.github.com>
2023-04-08 13:37:15 +0800

    添加了查询源代码Bug和使用自然语言执行命令的功能

--- chatgpt.py
+++ chatgpt.py
@@ -172,8 +172,10 @@ if __name__ == "__main__":
         chat [message] 与ChatGPT进行对话（支持连续对话）
         exit 退出ChatGPT Shell
         help 查看帮助信息
+        exec [message] 使用自然语言的方式执行命令：例如：exec 列出当前目录下的文件，包括隐藏文件，此时ChatGPT将会执行ls -a命令。
         history 查看历史命令
         search [command] 在ChatGPT Shell中搜索某个命令，例如：search command 将会搜索到command命令的使用/安装方法。
+        whatswrong [filename]  读取源代码文件，检查源代码的语法错误，并提供修复建议。
             """)
             elif user_input == "history":
                 for i in range(1, readline.get_current_history_length() + 1):
@@ -190,11 +192,47 @@ if __name__ == "__main__":
                     os.chdir(path)
                 else:
                     print(f"cd: {path}: 没有那个文件或目录")
+            elif user_input.startswith("whatswrong "):
+                # 判断文件是否存在
+                filename = user_input[11:]
+                if not os.path.exists(filename):
+                    print(f"whatswrong: {filename}: 没有那个文件或目录")
+                    continue
+                else:
+                    # 读取文件
+                    with open(filename, "r") as f:
+                        code = f.read()
+                    # 检查文件
+                    reply = chatgpt.send_message(f"我编写了一段源代码，但是不知道是否有语法错误，请你帮我检查一下。源代码内容：\n\n {code}")
+                    if reply is not None:
+                        print(reply)
+            elif user_input.startswith("exec "):
+                # 判断文件是否存在
+                message = user_input[5:]
+                reply = chatgpt.send_message(f"我想要{message}，请你直接给出命令，不要对内容进行复述，不要回复任何其他内容，由于我要将你的回答传递给计算机进行执行，如果你回答了其他内容，将会执行失败。注意：1. 如果命令必须以root身份执行，请在命令前加上sudo（例如某些发行版本使用包管理器安装应用）。\n2. 如果命令有危险性，请拒绝执行，并使用echo命令输出该命令存在的风险。\n3. 以下为示例：\n列出当前目录下的文件，包括隐藏文件\tls -a\n查看Linux内核版本\tuname -r\n切换为root用户\tsudo su")
+                if reply is not None:
+                    # 执行命令
+                    print(f"根据你的回复，我将执行命令：{reply}")
+                    try:
+                        result = subprocess.run(reply, shell=True)
+                        if result.returncode == 0:
+                            failed = False
+                        else:
+                            failed = True
+                    except subprocess.TimeoutExpired:
+                        print("命令执行超时")
+                        failed = True
+                    except FileNotFoundError:
+                        print("命令不存在")
+                        failed = True
+                    except Exception as e:
+                        print(e)
+                        failed = True
             else:
                 # 执行命令
                 try:
                     cmd = f"{user_input}"
-                    result = subprocess.run(user_input, shell=True)
+                    result = subprocess.run(cmd, shell=True)
                     if result.returncode == 0:
                         failed = False
                     else:
